eab4540569635ffaa655d3b42fbd361b40df85d1
DESKTOP-IFAK7EE\Hisyam <hisyamsyam90@gmail.com>
2024-05-10 00:30:37 +0800

    tbc hero

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 // import Head from "next/head";
 // import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Head><title>AppName</title></Head> */}
+      <Head><title>Pak Meet Western</title></Head>
       {/* <Analytics /> */}
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Footer from "@/components/Footer";
 import HeaderNavbar from "@/components/HeaderNavbar";
+import Hero from "@/components/Hero";
 
 const Home = () => {
   return (
     <>
-    <HeaderNavbar/>
-    <main className="flex flex-col items-center justify-center p-6 min-h-screen">
-      <p className="font-semibold text-5xl">if it works</p>
-      <p className="font-semibold text-3xl">it asd</p>
-    </main>
-    <Footer/>
+    <Hero></Hero>
     </>
   );
 }
